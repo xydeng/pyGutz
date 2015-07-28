6f1983b8f30fbcd09fbc8f4f2588c1895f1d4bda
Yongxin Yao <ykent@iastate.edu>
2015-07-28 01:05:19 -0400

    test_...py modified to get automatic GL.INP

--- test/square_lattice_2/REF_U0/test_square_lattice_2.py
+++ test/square_lattice_2/REF_U0/test_square_lattice_2.py
@@ -21,10 +21,9 @@ if __name__=="__main__":
     kps=kpoints.monkhorst_pack(kps_size)
     # unit cell
     ### a Gutz TB model on a square lattice.
-    gTB=tbGutz(sTB.Atoms,sTB.Hr,interaction=["Kanamori",(0.0,)])
+    gTB=tbGutz(sTB.Atoms,sTB.Hr,interaction=["Kanamori",(8.0,)])
     gTB.output_CyGutz(kps, num_electrons = 2.4)
 
-    # WH_HS.INP
     sigma_list = []; U_list = []
     norbitals = sTB.Atoms.nspinorbitals/2
     for i in range(1):
@@ -32,15 +31,5 @@ if __name__=="__main__":
       sigma_list.append(block_diag(sig_half, sig_half))  # assuming Sz conservation
       U_list.append(np.identity(norbitals*2, dtype = complex))
 
-    from gl_inp import set_wh_hs, set_gl_inp
+    from gl_inp import set_wh_hs
     set_wh_hs(sigma_list, U_list)
-
-    # GL.INP
-    spin_pol = 'n'
-    SOC = ['y']; CF = ['y']
-    NTYPE = 1; NIONS = 1; ITYPE_list = [1]
-    NI0_list = [1]; NIMAP_list = [1]; corr_atom_type = ["X"]
-    type_1atom = [0]; df_list = ["g"]; dim_list = [norbitals*2]
-    log = open("init_ga.slog", 'w')
-    set_gl_inp(spin_pol, SOC, CF, NTYPE, NIONS, ITYPE_list, NI0_list, NIMAP_list, corr_atom_type, type_1atom, df_list, dim_list, log)
-    log.close()
